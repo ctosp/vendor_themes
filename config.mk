25fae6d74e4cfc5902492b03153f96d8a2033958
Vamsee Krishnaa <vamsikrishna481998@gmail.com>
2019-05-08 18:43:05 +0530

    QS fixes

---
+++ config.mk
@@ -0,0 +1,63 @@
+PRODUCT_PACKAGES += \
+    SettingsDark \
+    SettingsIntelligenceDark \
+    SystemDark \
+    SystemUIDark \
+    GBoardDark \
+    GBoardLight \
+    WellbeingDark
+
+# QS tile styles
+PRODUCT_PACKAGES += \
+    QStileCircleTrim \
+    QStileDefault \
+    QStileDualToneCircle \
+    QStileSquircleTrim \
+    QStileAttemptMountain \
+    QStileCircleDualTone \
+    QStileCircleGradient \
+    QStileDottedCircle \
+    QStileNinja \
+    QStilePokesign \
+    QStileWavey \
+    QStileSquaremedo \
+    QStileOreo \
+    QStileOreoCircleTrim \
+    QStileOreoSquircleTrim \
+    QStileInkDrop \
+    QStileCookie \
+    QStileCircleOutline \
+    DefaultDarkTransparent-SystemUI \
+    DefaultDarkTransparent-System
+
+# Accent and Base packages
+PRODUCT_PACKAGES += \
+    AmberTheme \
+    BlueTheme \
+    CyanTheme \
+    DeepOrangeTheme \
+    DeepPurpleTheme \
+    GreenTheme \
+    IndigoTheme \
+    LightBlueTheme \
+    LightGreenTheme \
+    LimeTheme \
+    OrangeTheme \
+    PinkTheme \
+    PixelTheme \
+    PurpleTheme \
+    RedTheme \
+    StockTheme \
+    YellowTheme \
+    MintGreenTheme \
+    FadedPink \
+    DeepRed \
+    PinkRed \
+    KindaIndigo \
+    ArmyGreen \
+    Grey
+
+# QS Accent Packages
+PRODUCT_PACKAGES += \
+    QsAccentBlack \
+    QsAccentWhite
